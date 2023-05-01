810937ae7b6d85c1a64877ebe271b36ccb01255a
Liliana Franco <lilimafra@hotmail.com>
2023-04-30 20:00:33 -0500

    Ajuste_ Tarea4

--- js/task-04.js
+++ js/task-04.js
@@ -1,12 +1,9 @@
-// Obtener referencias a los elementos DOM
 const decrementButton = document.querySelector('[data-action="decrement"]');
 const incrementButton = document.querySelector('[data-action="increment"]');
 const valueElement = document.getElementById('value');
 
-// Inicializar el valor del contador
 let counterValue = 0;
 
-// Agregar escuchas de clic a los botones
 decrementButton.addEventListener('click', () => {
 counterValue--;
 valueElement.textContent = counterValue;
