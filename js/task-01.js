a973748e9154c4b9ecc5bf55578338d5714042d0
Liliana Franco <lilimafra@hotmail.com>
2023-04-30 23:47:00 -0500

    Ajuste_Tarea01

--- js/task-01.js
+++ js/task-01.js
@@ -2,4 +2,4 @@ const categoriesList = document.querySelector("categories");
 const categoriesItems = categoriesList.querySelectorAll("li.item");
 categoriesItems.forEach((categoryItem) => {
 const title = categoryItem.querySelector("h2").textContent;
-const articles = categoryItem.querySelectorAll("li").length;
+    const articles = categoryItem.querySelectorAll("li").length});
